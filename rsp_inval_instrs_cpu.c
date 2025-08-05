b060c5a00536ddd7134f4af148ca8bf5d43a040b
Pekka Väänänen <pekka@30fps.net>
2025-08-05 23:25:54 +0300

    a no op test

--- rsp_inval_instrs_cpu.c
+++ rsp_inval_instrs_cpu.c
@@ -25,8 +25,8 @@ int main(void)
         if (status & (SP_STATUS_HALTED | SP_STATUS_BROKE | SP_STATUS_SIG2 | SP_STATUS_SIG3)) break;
     }
 
-    rsp_snapshot_t before={};
-    rsp_snapshot_t after={};
+    rsp_snapshot_t before __attribute__((aligned(8)))={};
+    rsp_snapshot_t after __attribute__((aligned(8)))={};
     // align to 8 bytes
 
     rsp_read_data(&before, 764, 0);
@@ -35,16 +35,76 @@ int main(void)
     //memset(after.dmem, 0, 4096);
     rsp_read_data(&after.dmem, 4096, 0);
 
+// typedef struct {
+//     uint32_t gpr[32];           ///< General purpose registers
+//     uint16_t vpr[32][8];        ///< Vector registers
+//     uint16_t vaccum[3][8];      ///< Vector accumulator
+//     uint32_t cop0[16];          ///< COP0 registers (note: reg 4 is SP_STATUS)
+//     uint32_t cop2[3];           ///< COP2 control registers
+//     uint32_t pc;                ///< Program counter
+//     uint8_t dmem[4096] __attribute__((aligned(8)));  ///< Contents of DMEM
+//     uint8_t imem[4096] __attribute__((aligned(8)));  ///< Contents of IMEM
+// } rsp_snapshot_t;
+
     // debugf("DMEM:\n");
     // debug_hexdump(after.dmem, 100);
     int diffs=0;
-    for (int i=0;i<764;i++) {
+    int cop0diffs = 0;
+    for (int i=0;i<offsetof(rsp_snapshot_t, pc);i++) {
         uint8_t* a = (uint8_t*)&before;
         uint8_t* b = (uint8_t*)&after;
         if (a[i] != b[i]) {
-            debugf("[0x%04x] before=%02x vs after=%02x\n", i,a[i],b[i]);
+            debugf("[0x%04x] before=%02x vs after=%02x. DMEM[word=%d]\n", i,a[i],b[i],i/4);
+            if (i < offsetof(rsp_snapshot_t, vpr)) {
+                int idx = i/4;
+                debugf("gpr %d\n", idx);
+            } else if (i < offsetof(rsp_snapshot_t, vaccum)) {
+                int idx = (i - offsetof(rsp_snapshot_t, vpr))/16;
+                debugf("vpr %d\n", idx);
+            }else if (i < offsetof(rsp_snapshot_t, cop0)) {
+                int idx = (i - offsetof(rsp_snapshot_t, vaccum))/16;
+                debugf("vaccum %d\n", idx);
+            } else if ( i < offsetof(rsp_snapshot_t, cop2)) {
+                int idx = (i - offsetof(rsp_snapshot_t, cop0))/4;
+                debugf("cop0 %d\n", idx);
+                cop0diffs++;
+
+                
+                
+            } else if ( i < offsetof(rsp_snapshot_t, pc)) {
+                int idx = (i - offsetof(rsp_snapshot_t, cop2))/4;
+                debugf("cop2 %d\n", idx);
+            }
             diffs++;
         }
+    }
+
+    if (cop0diffs > 0) { 
+                debugf("  before vs after\n");
+                // print before vs after cop0 registers in their own two columns
+                // before.cop0 and after.cop0
+                const char* cop0names[] = {
+                    "COP0_DMA_SPADDR",
+                    "COP0_DMA_RAMADDR",
+                    "COP0_DMA_READ",
+                    "COP0_DMA_WRITE",
+                    "COP0_SP_STATUS",
+                    "COP0_DMA_FULL",
+                    "COP0_DMA_BUSY",
+                    "COP0_SEMAPHORE",
+                    "COP0_DP_START",
+                    "COP0_DP_END",
+                    "COP0_DP_CURRENT",
+                    "COP0_DP_STATUS",
+                    "COP0_DP_CLOCK",
+                    "COP0_DP_BUSY",
+                    "COP0_DP_PIPE_BUSY",
+                    "COP0_DP_TMEM_BUSY"
+                };
+
+                for (int j=0;j<16;j++) {
+                    debugf("[%18s (%02d)] 0x%08lx 0x%08lx%s\n", cop0names[j], j, before.cop0[j], after.cop0[j], (before.cop0[j] != after.cop0[j]) ? " <--" : "");
+                }
     }
 
     if (diffs > 0) {
@@ -52,9 +112,9 @@ int main(void)
     }
 
     debugf("Before:\n");
-    debug_hexdump(&before, 100);
+    debug_hexdump(&before, 200);
     debugf("After:\n");
-    debug_hexdump(&after, 100);
+    debug_hexdump(&after, 200);
 
     if((status & SP_STATUS_SIG2)){
         debugf("Test passed");
